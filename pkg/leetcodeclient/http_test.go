5201149e6890aefa258716cec73c6ec5484ecd10
Sergey Mishin <dartkron@users.noreply.github.com>
2024-12-09 03:04:04 +0000

    Fix csrf token in test

--- pkg/leetcodeclient/http_test.go
+++ pkg/leetcodeclient/http_test.go
@@ -27,7 +27,7 @@ func TestRequestGraphQl(t *testing.T) {
 		"Content-Type": []string{"application/json"},
 		"Accept":       []string{"*/*"},
 		"User-Agent":   []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"},
-		"X-Csrftoken":  []string{"Lwf3m4FIFmw8a9u8RQ3u8vXQq7Zqra3aWfBJT8PvXAjhex3V8IQulH1EmXFmKEk8"},
+		"X-Csrftoken":  []string{"AhmTr4FHZa0sXagk4bEdWYRr50fK5BKa1a0F2ybPwBpoEWuNhKNKmMWlfaCjgCuq"},
 	}
 
 	httpTransportMock.On(
